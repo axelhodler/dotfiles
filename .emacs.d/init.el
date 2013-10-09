81e1a7261d62eec2f75272031d37d09f70105807
xorrr <derxor@gmail.com>
2013-10-09 11:52:31 +0200

    merge .emacs and .emacs.d/init.el then remove .emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,21 @@
+(require 'package)
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Add in your own as you wish:
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings starter-kit-js graphviz-dot-mode color-theme-solarized)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(load-theme 'solarized-dark t)
+
 ;; Auctex
 (load "auctex.el" nil t t)
 (load "preview-latex.el" nil t t)
@@ -32,5 +50,4 @@
 ;; Make org-mode work with files ending in .org
 ;; (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 ;; The above is the default in recent emacsen
-
 
