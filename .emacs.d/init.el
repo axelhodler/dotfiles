6c54a16d3d33cf0cf3a11ff1e7494c5be11900fe
xorrr <derxor@gmail.com>
2013-11-07 23:24:28 +0100

    add the marmalade repo to emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -20,3 +20,12 @@
 
 ;; remove trailing whitespaces when saving
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; add the marmelade repository
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
