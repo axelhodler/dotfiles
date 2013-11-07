eacda2ad9fe3e6eb34f0cf4e53d11a9ace9d0cb6
xorrr <derxor@gmail.com>
2013-11-07 23:18:56 +0100

    add ido M-x mode to emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,6 +2,17 @@
 (require 'ido)
 (ido-mode t)
 
+;; M-x mode, autocomplete M-x methods
+(global-set-key
+     "\M-x"
+     (lambda ()
+       (interactive)
+       (call-interactively
+        (intern
+         (ido-completing-read
+          "M-x "
+          (all-completions "" obarray 'commandp))))))
+
 ;; stop creating those backup~ files
 (setq make-backup-files nil)
 ;; stop creating those #auto-save# files
