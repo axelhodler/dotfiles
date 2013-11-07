b27ea1ed9b1033765bc718d1d1bea378da90a82f
xorrr <derxor@gmail.com>
2013-11-07 22:17:21 +0100

    deal with backup and autosave files in emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,8 @@
 ;; Interactively Do Things
 (require 'ido)
 (ido-mode t)
+
+;; stop creating those backup~ files
+(setq make-backup-files nil)
+;; stop creating those #auto-save# files
+(setq auto-save-default nil)
