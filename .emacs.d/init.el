5f9e6a687771b170d1c7364b2d1868001a9a3ba3
xorrr <derxor@gmail.com>
2013-11-08 20:33:04 +0100

    use solarized theme in emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -74,3 +74,6 @@
 ;; Font
 (set-face-font 'default "-unknown-Ubuntu Mono-bold-normal-normal-*-18-*-*-*-m-0-iso10646-1")
 (set-face-font 'menu "-unknown-Ubuntu Mono-normal-normal-normal-*-18-*-*-*-m-0-iso10646-1")
+
+;; Use solarized theme
+(load-theme 'solarized-dark t)
