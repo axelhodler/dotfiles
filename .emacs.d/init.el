1efb801b9c07142bb0220b68e52d93cc8e2ce67e
xorrr <derxor@gmail.com>
2013-11-08 20:18:27 +0100

    add js2-refactor to emacs and set keybindings prefix

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -45,6 +45,10 @@
 ;; use js2-mode for .js files
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
+(require 'js2-refactor)
+;; eg. extract function with `C-c C-m ef`.
+(js2r-add-keybindings-with-prefix "C-c C-m")
+
 ;; autocomplete
 ;; concerning javascript you need to copy javascript-mode to js-mode since
 ;; emacs seems to use js-mode by default and AC will therefore try to use
