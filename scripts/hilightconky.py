a363726a07aeb9de4a209010c0a7e08d35d37fa5
xorrr <derxor@gmail.com>
2013-11-11 14:02:42 +0100

    change file to write into and string to write

--- scripts/hilightconky.py
+++ scripts/hilightconky.py
@@ -6,6 +6,6 @@ match = re.search(r'<(.+)>', sys.argv[1])
 user = match.group(1)
 
 # write the hilight to file
-f = open('hilight', 'w')
-f.write(">>> " + chan + " <<<\n")
+f = open('/home/xorrr/dotfiles/scripts/hilight', 'w')
+f.write(user + " @IRC\n")
 f.close()
