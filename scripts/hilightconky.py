9082bad7977aa7b30ed3c3c994bac0654fc12396
xorrr <derxor@gmail.com>
2014-01-05 05:45:02 +0100

    refactor highlightconky script

--- scripts/hilightconky.py
+++ scripts/hilightconky.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 import sys, re
 
+FILENAME = '/home/xorrr/dotfiles/scripts/hilight'
+
 # format the string
 match = re.search(r'<(.+)>', sys.argv[1])
 user = match.group(1)
 
 # write the hilight to file
-f = open('/home/xorrr/dotfiles/scripts/hilight', 'w')
-f.write(user + " @IRC\n")
+f = open(FILENAME, 'w')
+f.write(user)
 f.close()
