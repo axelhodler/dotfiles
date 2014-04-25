c0a485b63437908663ff18e7f6f7361345004456
xorrr <derxor@gmail.com>
2014-04-25 16:38:50 +0200

    emacs: configure markdown mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -125,3 +125,10 @@
 
 ;; auto refresh buffer if file has changed
 (global-auto-revert-mode t)
+
+;; markdown mode
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
