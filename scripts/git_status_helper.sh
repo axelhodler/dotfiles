60a77b2085fff1176e39e4e5ffabd8994941ead8
xorrr <derxor@gmail.com>
2015-01-08 00:11:55 +0100

    scripts: initial git status helper

---
+++ scripts/git_status_helper.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+COUNT=0
+
+git config color.status always
+
+git status -sb |
+  while read line;
+    do
+      if [ "$COUNT" -ge 1 ]; then
+        echo $line | sed -e "s#^#[ $COUNT ]#g"
+      else
+        echo $line
+      fi
+      (( COUNT++ ))
+    done
