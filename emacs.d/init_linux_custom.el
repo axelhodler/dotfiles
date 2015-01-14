cda0686a26beb33cdabf15faa6c13749d8eb0018
xorrr <derxor@gmail.com>
2015-01-14 23:33:08 +0100

    emacs: change fontsize

--- emacs.d/init_linux_custom.el
+++ emacs.d/init_linux_custom.el
@@ -1,3 +1,2 @@
 ;; Font
-(set-face-font 'default "-unknown-Ubuntu Mono-bold-normal-normal-*-18-*-*-*-m-0-iso10646-1")
-(set-face-font 'menu "-unknown-Ubuntu Mono-normal-normal-normal-*-18-*-*-*-m-0-iso10646-1")
+(set-default-font "Monaco for Powerline-13")
