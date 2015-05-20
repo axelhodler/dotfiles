ab5a0b825004792e9e3900bcb92fe61b9a1af21f
xorrr <derxor@gmail.com>
2015-05-20 23:47:13 +0200

    git: add script to prepend the jira ticket / branchname to the commitmsg

---
+++ scripts/git_jira_ticket_prepender.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+commit_msg="$@"
+echo $commit_msg
+
+branchname=$(git rev-parse --abbrev-ref HEAD)
+
+# if branches are called feature/relution/MDM-1337
+ticketid=$(echo $branchname | cut -f3 -d"/")
+
+git commit -m "$ticketid $commit_msg"
