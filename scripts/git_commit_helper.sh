5912724b207cd8aaec80ec4cf09a62f54ed45157
xorrr <derxor@gmail.com>
2015-09-27 13:44:33 +0200

    add char count checker to commit msg

---
+++ scripts/git_commit_helper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+MAX_CHARACTERS=50
+
+commit_msg="$@"
+
+used_characters=$(printf "$commit_msg" | wc -c)
+
+if [ "$used_characters" -le "$MAX_CHARACTERS" ]
+then
+  git commit -m "$commit_msg"
+else
+  chars_over_max=$(expr $used_characters - $MAX_CHARACTERS)
+  printf "The chosen commit message contains $chars_over_max chars too much.\nPlease choose a shorter message\n"
+fi
