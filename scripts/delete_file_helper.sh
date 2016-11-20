1516ea8983d00f7982eab85bb044379431350177
Axel Hodler <derxor@gmail.com>
2016-11-20 10:24:43 +0100

    tell user why it fails

--- scripts/delete_file_helper.sh
+++ scripts/delete_file_helper.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+if ! hash trash 2>/dev/null; then
+  echo "run npm install -g trash trash-cli first to install necessary dependencies"
+  exit 1
+fi
+
 files=()
 
 source ~/dotfiles/scripts/git_helper_statusreader.sh
 
 for filenumber in "$@"
   do
-    # npm install -g trash
     trash ${files[$filenumber]}
   done
