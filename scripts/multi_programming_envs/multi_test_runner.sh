3717a834b6b9c56619dc5fa34efc914315ffc071
Axel Hodler <derxor@gmail.com>
2017-02-12 09:22:34 +0100

    support stack based haskell projects

--- scripts/multi_programming_envs/multi_test_runner.sh
+++ scripts/multi_programming_envs/multi_test_runner.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ -f stack.yaml ];
+then
+  stack repl <<< test
+fi
+
 if [ -f package.json ];
 then
   npm test
--- scripts/multi_programming_envs/test_multi_test_runner.sh
+++ scripts/multi_programming_envs/test_multi_test_runner.sh
@@ -17,6 +17,14 @@ flunk() {
 (
   cd maven
   sh ../multi_test_runner.sh > output 2>&1
-  [ `cat output | grep -c "npm test"` == 0 ] || flunk "npm test should not have been invoked in a java project"
+  [ `cat output | grep --count "npm test"` == 0 ] || flunk "npm test should not have been invoked in a java project"
+  rm output
+)
+
+(
+  cd stack
+  sh ../multi_test_runner.sh > output 2>&1
+  [ `cat output | grep --count "Leaving GHCi"` == 1 ] || flunk "stack was not invoked"
+  [ `cat output | grep --count "Variable not in scope: test"` == 1 ] || flunk "test was not invoked"
   rm output
 )
