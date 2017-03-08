01a0a05313e8c9665e4718208c99a2d5393b7425
Axel Hodler <derxor@gmail.com>
2017-03-08 07:15:25 +0100

    parity needs content type

--- scripts/ethereum_helper_functions.sh
+++ scripts/ethereum_helper_functions.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
+function isChainRunning() {
+  curl --silent localhost:8545 > /dev/null
+  if [ $? -ne 0 ]; then
+    echo "Could not connect to ethereum blockchain"
+  fi
+}
+
 function gasLimit() {
-  curl --silent -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", true],"id":1}' localhost:8545 | jq .result.gasLimit | xargs printf '%d\n'
+  isChainRunning
+  curl --silent -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", true],"id":1}' localhost:8545 | jq .result.gasLimit | xargs printf '%d\n'
 }
 
 function gasPrice() {
-  curl --silent -X POST --data '{"jsonrpc":"2.0","method":"eth_gasPrice","params":[],"id":73}' localhost:8545 | jq .result | xargs printf '%d\n'
+  curl --silent -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_gasPrice","params":[],"id":73}' localhost:8545 | jq .result | xargs printf '%d\n'
 }
 
 function accounts() {
-  curl --silent -X POST --data '{"jsonrpc":"2.0","method":"eth_accounts","id":1}' localhost:8545 | jq .result
+  curl --silent -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_accounts","id":1}' localhost:8545 | jq .result
 }
