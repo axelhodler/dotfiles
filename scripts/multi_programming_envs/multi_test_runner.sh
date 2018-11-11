ff3fdbae2afaddf1772196901dcaa66413870b94
Axel Hodler <axel@hodler.co>
2018-11-11 10:20:49 +0100

    support kotlin gradle files

--- scripts/multi_programming_envs/multi_test_runner.sh
+++ scripts/multi_programming_envs/multi_test_runner.sh
@@ -5,12 +5,12 @@ then
   stack repl <<< test
 fi
 
-if [ -f package.json ] && [ ! -f build.gradle ];
+if [ -f package.json ] && [ ! -f build.gradle ] && [ ! -f build.gradle.kts ];
 then
   npm test
 fi
 
-if [ -f build.gradle ];
+if [ -f build.gradle ] || [ -f build.gradle.kts ];
 then
   ./gradlew test
 fi
