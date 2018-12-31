a2d131d08f160dfc8c712f723912abffd6dbc9fd
Axel Hodler <axel@hodler.co>
2018-12-31 09:47:10 +0100

    describe what its doing

--- scripts/multi_programming_envs/multi_test_runner.sh
+++ scripts/multi_programming_envs/multi_test_runner.sh
@@ -7,15 +7,18 @@ fi
 
 if [ -f package.json ] && [ ! -f build.gradle ] && [ ! -f build.gradle.kts ];
 then
+  echo "Recognizing npm project, running npm test"
   npm test
 fi
 
 if [ -f build.gradle ] || [ -f build.gradle.kts ];
 then
+  echo "Recognizing gradle project, running ./gradlew test"
   ./gradlew test
 fi
 
 if [ -f pom.xml ];
 then
+  echo "Recognizing maven project, running mvn test"
   mvn test
 fi
